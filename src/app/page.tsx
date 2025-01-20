fd025e7db695dc2c5b93b6b8d05b06224d8b8c49
Tomohiko Ikeda <tom-e-kid@users.noreply.github.com>
2025-01-20 10:55:42 +0900

    refactor: default lang to ja

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import { redirect } from 'next/navigation';
+import { redirect } from "next/navigation";
 
-export const dynamic = 'force-static';
+export const dynamic = "force-static";
 
 export default function RootPage() {
-  redirect('/en');
+  redirect("/ja");
 }
